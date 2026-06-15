4ac3c28d5ff51df9bcb13b9e4830bfc59a0d540b
Lokesh19-2005 <YOUR_GITHUB_EMAIL>
2026-06-15 16:15:15 +0530

    Preloader: white circle bg for logo

--- src/components/PreLoader.tsx
+++ src/components/PreLoader.tsx
@@ -55,7 +55,7 @@ export default function PreLoader() {
             />
 
             {/* Logo */}
-            <div className="w-[120px] h-[120px] rounded-full bg-gray-900 border border-gray-700 p-5 shadow-2xl">
+            <div className="w-[120px] h-[120px] rounded-full bg-white p-5 shadow-2xl shadow-gold-500/20">
               <img
                 src="/logo-mark.png"
                 alt="Deekshi Global Exim"
